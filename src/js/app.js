618df8eebf820e9b920e58f32ad64e21ffa766b1
alexey <alexey.pytalev@gmail.com>
2021-04-24 17:47:15 +0300

    roadmap

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,8 @@
-
-
-
+document.addEventListener('DOMContentLoaded', function (event) {
+    var swiper = new Swiper('#roadmap-swiper', {
+        slidesPerView: 1,
+        spaceBetween: 80,
+        mousewheel: true,
+        slideToClickedSlide: true,
+    });
+});
